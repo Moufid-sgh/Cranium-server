5e24e46d631b17656634898320dde6c199962874
Moufid <moufid.sgh@protonmail.com>
2024-01-30 18:11:08 +0100

    engine

--- db.js
+++ db.js
@@ -1,13 +1,7 @@
 const { Pool } = require('pg')
 require("dotenv").config()
 
-// const pool = new Pool({
-//     user: process.env.USER, 
-//     password: process.env.PWD,
-//     host: process.env.HOST,
-//     database: process.env.DATABSE,
-//     port: 5432
-// })
+
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
   })
